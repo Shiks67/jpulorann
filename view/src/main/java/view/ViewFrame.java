47edb99fd8fcbb41b86af093cf8bd884ec12875a
me-cedric <mecedricod@gmail.com>
2016-06-14 16:14:20 +0200

    add pngs and correct display size

--- view/src/main/java/view/ViewFrame.java
+++ view/src/main/java/view/ViewFrame.java
@@ -131,7 +131,7 @@ class ViewFrame extends JFrame implements KeyListener {
 		this.setResizable(false);
 		this.addKeyListener(this);
 		this.setContentPane(new ViewPanel(this));
-		this.setSize(1600,900);
+		this.setSize(648,413);
 		this.setLocationRelativeTo(null);
 	}
 
