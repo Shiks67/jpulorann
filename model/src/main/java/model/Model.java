be176f3f18a6e38bf6a8d37e39c878b9f6e3f63a
Loic <oulerich.loic@viacesi.fr>
2016-06-17 13:53:28 +0200

    Kill all monster

--- model/src/main/java/model/Model.java
+++ model/src/main/java/model/Model.java
@@ -309,6 +309,24 @@ public class Model extends Observable implements IModel {
             getMonster1().setX(0);
             getMonster1().setY(0);
         }
+        else if (getShoot().getX() == getMonster2().getX() && getShoot().getY() == getMonster2().getY()) {
+            pngArray[getMonster2().getY()][getMonster2().getX()] = ' ';
+            canShoot = 1;
+            getMonster2().setX(0);
+            getMonster2().setY(0);
+        }
+        else if (getShoot().getX() == getMonster3().getX() && getShoot().getY() == getMonster3().getY()) {
+            pngArray[getMonster3().getY()][getMonster3().getX()] = ' ';
+            canShoot = 1;
+            getMonster3().setX(0);
+            getMonster3().setY(0);
+        }
+        else if (getShoot().getX() == getMonster4().getX() && getShoot().getY() == getMonster4().getY()) {
+            pngArray[getMonster4().getY()][getMonster4().getX()] = ' ';
+            canShoot = 1;
+            getMonster4().setX(0);
+            getMonster4().setY(0);
+        }
     }
 
     public void lastHP(){
