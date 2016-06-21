12f704cff39ea0a6a274ac764620f48eebec2ca0
Sylvain <sylvain.grimm@viacesi.fr>
2016-06-21 09:58:51 +0200

    Modify unit tests

--- model/src/test/java/element/mobile/HeroTest.java
+++ model/src/test/java/element/mobile/HeroTest.java
@@ -3,43 +3,61 @@ package element.mobile;
 import junit.framework.TestCase;
 
 /**
- * Created by Sylvain on 20/06/2016.
+ * Created by Sylvain on 21/06/2016.
  */
 public class HeroTest extends TestCase {
-    public void testGetX() throws Exception {
+    private Hero hero;
+    public void setUp() throws Exception {
+        super.setUp();
+        hero = new Hero(1,1);
+    }
 
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testGetX() throws Exception {
+    assertEquals(1,hero.getX());
     }
 
     public void testSetX() throws Exception {
-
+        hero.setX(2);
+        assertEquals(2,hero.getX());
     }
 
     public void testGetY() throws Exception {
-
+        assertEquals(1,hero.getY());
     }
 
     public void testSetY() throws Exception {
-
+        hero.setY(2);
+        assertEquals(2,hero.getY());
     }
 
     public void testMoveUp() throws Exception {
-
+        hero.moveUp();
+        assertEquals(0,hero.getY());
     }
 
     public void testMoveLEFT() throws Exception {
-
+        hero.moveLEFT();
+        assertEquals(0,hero.getX());
     }
 
     public void testMoveDOWN() throws Exception {
-
+        hero.moveDOWN();
+        assertEquals(2,hero.getY());
     }
 
     public void testMoveRIGHT() throws Exception {
-
+        hero.moveRIGHT();
+        assertEquals(2,hero.getX());
     }
 
     public void testHeroDeath() throws Exception {
-
+        hero.heroDeath();
+        assertEquals(-5,hero.getX());
+        assertEquals(-5,hero.getY());
     }
 
 }
--- model/src/test/java/element/mobile/ShootTest.java
+++ model/src/test/java/element/mobile/ShootTest.java
@@ -3,39 +3,55 @@ package element.mobile;
 import junit.framework.TestCase;
 
 /**
- * Created by Sylvain on 20/06/2016.
+ * Created by Sylvain on 21/06/2016.
  */
 public class ShootTest extends TestCase {
-    public void testGetX() throws Exception {
+    private Shoot shoot;
+    public void setUp() throws Exception {
+        super.setUp();
+        shoot = new Shoot(1,1);
+    }
 
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testGetX() throws Exception {
+        assertEquals(1,shoot.getX());
     }
 
     public void testSetX() throws Exception {
-
+        shoot.setX(2);
+        assertEquals(2,shoot.getX());
     }
 
     public void testGetY() throws Exception {
-
+        assertEquals(1,shoot.getY());
     }
 
     public void testSetY() throws Exception {
-
+        shoot.setY(2);
+        assertEquals(2,shoot.getY());
     }
 
     public void testMoveUP() throws Exception {
-
+        shoot.moveUP();
+        assertEquals(0,shoot.getY());
     }
 
     public void testMoveLEFT() throws Exception {
-
+        shoot.moveLEFT();
+        assertEquals(0,shoot.getX());
     }
 
     public void testMoveDOWN() throws Exception {
-
+        shoot.moveDOWN();
+        assertEquals(2,shoot.getY());
     }
 
     public void testMoveRIGHT() throws Exception {
-
+        shoot.moveRIGHT();
+        assertEquals(2,shoot.getX());
     }
 
 }
